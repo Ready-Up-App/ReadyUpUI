1ee15605110297c1c77e460e162935ae4a541af3
Ryan Sloan <ras18e@my.fsu.edu>
2022-04-30 20:38:43 -0400

    Added different navigation bar

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -1,19 +1,55 @@
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import Home from "../Screens/Home";
-import CreateActivity from "../Screens/CreateActivity";
 
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const Tabs = () => {
     return(
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Fuckyu" component={Home}></Tab.Screen>
-            <Tab.Screen name="Fuckme" component={CreateActivity}></Tab.Screen>
+        <Tab.Navigator
+            initialRouteName="Home"
+            barStyle={{ backgroundcolor: "#694fad" }}
+            activeColor="#f0edf6"
+            inactiveColor="#3e2465"
+            >
+
+            <Tab.Screen name="Home" component={Home} 
+            options={{
+                tabBarIcon : ({ color, size}) => (
+                    <MaterialCommunityIcons name="home" color={color} size={26}/>
+                )
+            }}
+            />
+
+        <Tab.Screen name="Test" component={Home} 
+            options={{
+                tabBarIcon : ({ color, size}) => (
+                    <MaterialCommunityIcons name="magnify" color={color} size={26}/>
+                )
+            }}
+            />
         </Tab.Navigator>
     );
 }
+
+
+const screenOptions = (route, color) => {
+    let iconName;
+
+    switch(route.name){
+        case "Home":
+            iconName = "home";
+            break;
+        case "Test":
+            iconName = "folder1";
+            break;
+    }
+
+    return <Icon name={iconName} color={color} size={24}/>;
+}
+
 
 export default Tabs;
